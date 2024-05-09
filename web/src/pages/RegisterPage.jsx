b20a59442b3cab25ce13a414409151b333cfb2d8
nawad0 <doarmax.max@gmail.com>
2024-05-09 23:58:55 +0300

    test

--- web/src/pages/RegisterPage.jsx
+++ web/src/pages/RegisterPage.jsx
@@ -47,7 +47,7 @@ const RegisterPage = () => {
 		handleBoolFalse();
 
 		toast.promise(
-			axios.post('http://localhost:5177/api/auth/register', {
+			axios.post('http://83.229.83.240:5177/api/auth/register', {
 				username,
 				email,
 				password,

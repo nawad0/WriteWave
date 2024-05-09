3b8b2a5b08f42d00036647dec32476d7e0b49d5a
nawad0 <doarmax.max@gmail.com>
2024-05-10 00:42:09 +0300

    url

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+window.apiUrl = "http://83.229.83.240:5177";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <App />
